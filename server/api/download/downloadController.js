6a79df293314a25d79eff59fa900582ee33398ec
UsamaAslam <uaslam345@gmail.com>
2020-12-29 01:00:27 +0500

    indegoModel and schema design

--- server/api/download/downloadController.js
+++ server/api/download/downloadController.js
@@ -1,7 +1,9 @@
+
+
 class Download {
   fetchData(req, res) {
     try {
-      res.send("hello world");
+
     } catch (error) {
       res.status(400).send({ error: { message: error.message } });
     }
---
+++ server/model/indegoModel.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const indegoDataSchema = new mongoose.Schema({
+  geometry: {
+    coordinates: [{ type: Number }],
+    type: { type: String },
+  },
+  properties: {
+    id: { type: Number },
+    name: { type: String },
+    coordinates: [{ type: Number }],
+    totalDocks: { type: Number },
+    docksAvailable: { type: Number },
+    bikesAvailable: { type: Number },
+    classicBikesAvailable: { type: Number },
+    smartBikesAvailable: { type: Number },
+    electricBikesAvailable: { type: Number },
+    rewardBikesAvailable: { type: Number },
+    rewardDocksAvailable: { type: Number },
+    kioskStatus: { type: String },
+    kioskPublicStatus: { type: String },
+    kioskConnectionStatus: { type: String },
+    kioskType: { type: Number },
+    addressStreet: { type: String },
+    addressCity: { type: String },
+    addressState: { type: String },
+    addressZipCode: { type: String },
+    bikes: [
+      {
+        dockNumber: { type: Number },
+        isElectric: { type: Boolean },
+        isAvailable: { type: Boolean },
+        battery: { type: Number },
+      },
+    ],
+    closeTime: { type: mongoose.Schema.Types.Mixed },
+    eventEnd: { type: mongoose.Schema.Types.Mixed },
+    eventStart: { type: mongoose.Schema.Types.Mixed },
+    isEventBased: { type: Boolean },
+    isVirtual: { type: Boolean },
+    kioskId: { type: Number },
+    notes: { type: mongoose.Schema.Types.Mixed },
+    openTime: { type: mongoose.Schema.Types.Mixed },
+    publicText: { type: mongoose.Schema.Types.Mixed },
+    timeZone: { type: mongoose.Schema.Types.Mixed },
+    trikesAvailable: { type: Number },
+    latitude: { type: Number },
+    longitude: { type: Number },
+  },
+  type: { type: String },
+});
+
+const IndegoDataModel = mongoose.model("indegoData", indegoDataSchema);
+
+module.exports = { IndegoDataModel };
