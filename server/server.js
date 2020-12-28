d8228fb74811a493caaa802a131362e78fa41ab8
UsamaAslam <uaslam345@gmail.com>
2020-12-29 00:16:13 +0500

    router setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const { mongoose } = require("./db/mongoose");
+const bodyParser = require("body-parser");
+const api = require("./api/index");
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/api/v1", api);
 
 const PORT = process.env.PORT || 4000;
 
