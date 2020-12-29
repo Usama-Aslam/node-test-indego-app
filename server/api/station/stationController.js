530891a72b9d3e532b54de449eb5a22fdd11a4d2
UsamaAslam <uaslam345@gmail.com>
2020-12-29 19:00:05 +0500

    getStationDataByID API done

--- server/api/station/stationController.js
+++ server/api/station/stationController.js
@@ -14,7 +14,7 @@ class Station {
       const getWeather = axios.get(url);
 
       const getStation = IndegoDataModel.find({
-        createdAt: { $gte: at, $lt: dayjs().add(1, "m") },
+        createdAt: { $gte: at, $lte: dayjs().add(1, "m") },
       }).sort({ createdAt: 1 });
 
       //parallel execution of request
@@ -29,6 +29,39 @@ class Station {
 
       res.status(200).send({
         at: stationData[0].createdAt,
+        stationData,
+        weatherData: weatherData.data,
+      });
+    } catch (error) {
+      res.status(404).send({ error: { message: error.message } });
+    }
+  }
+
+  async getStationDataByID(req, res) {
+    try {
+      const { id } = req.params;
+      const { at } = req.query;
+
+      const url = `${openWeatherAPI}?q=Philadelphia&appid=${process.env.openWeatherAPI}`;
+
+      //fetching weatherData and stationData
+      const getWeather = axios.get(url);
+
+      const getStation = IndegoDataModel.findOne({
+        "properties.kioskId": id,
+        createdAt: at,
+      });
+
+      //parallel execution of request
+      const data = await Promise.allSettled([getStation, getWeather]);
+
+      const stationData = data[0].value;
+      const weatherData = data[1].value;
+
+      if (!stationData) throw new Error("data not found");
+
+      res.status(200).send({
+        at: stationData.createdAt,
         stationData,
         weatherData: weatherData.data,
       });
--- server/api/station/stationRoute.js
+++ server/api/station/stationRoute.js
@@ -3,5 +3,6 @@ const stationController = require("./stationController");
 const stationCtrl = new stationController();
 
 router.get("", stationCtrl.getStationData);
+router.get("/:id", stationCtrl.getStationDataByID);
 
 module.exports = router;
